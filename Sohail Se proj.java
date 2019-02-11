7e76c3a127340d4d7e0fd6c198f977f24fe3ef94
sohail124 <47071815+sohail124@users.noreply.github.com>
2019-02-12 01:54:09 +0500

    Update Sohail Se proj.java

--- Sohail Se proj.java
+++ Sohail Se proj.java
@@ -1,12 +1,16 @@
 class Data{	
 void studentData(){		
-java.util.Scanner in = new java.util.Scanner(System.in);		System.out.println("Talha's ID is 11");		System.out.println("Sohail's ID is 12");	
+java.util.Scanner in = new java.util.Scanner(System.in);		
+System.out.println("Talha's ID is 11");		
+System.out.println("Sohail's ID is 12");	
 System.out.println("Mansoor's ID is 13");	
 System.out.print("Enter Student ID:");		
 int id = in.nextInt();			
 if (id == 12){			
 String name = "Sohail";			
-String rollno = "2k17/csee/74";			System.out.println("Name:"+name);			System.out.println("Roll no is:"+rollno);			
+String rollno = "2k17/csee/74";			
+System.out.println("Name:"+name);		
+System.out.println("Roll no is:"+rollno);			
 }			
 }	
 public static void main(String str[]){		
