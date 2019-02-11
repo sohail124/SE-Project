a9d06d604f59c923d7910d49b14e16ec1b532f0c
sohail124 <47071815+sohail124@users.noreply.github.com>
2019-02-12 01:57:37 +0500

    Update Sohail Se proj.java

--- Sohail Se proj.java
+++ Sohail Se proj.java
@@ -5,7 +5,14 @@ System.out.println("Talha's ID is 11");
 System.out.println("Sohail's ID is 12");	
 System.out.println("Mansoor's ID is 13");	
 System.out.print("Enter Student ID:");		
-int id = in.nextInt();			
+int id = in.nextInt();	
+ if (id == 11){			
+String name = "Talha";			
+String rollno = "2k17/csee/75";			
+System.out.println("Name:"+name);		
+System.out.println("Roll no is:"+rollno);			
+}			
+}	
 if (id == 12){			
 String name = "Sohail";			
 String rollno = "2k17/csee/74";			
